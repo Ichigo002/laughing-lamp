427b2d2bf89346bfeb72da1f653e5cb5a1d3758a
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2022-09-04 22:42:20 +0200

    move everything into msvc2019

---
+++ laughing-lamp/src/cmd/SimpleCommand.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../Game.h"
+
+#ifndef SIMPLECOMMAND_H
+#define SIMPLECOMMAND_H
+
+class SimpleCommand
+{
+public:
+    SimpleCommand() { }
+    virtual ~SimpleCommand() { }
+
+    std::string* overloads;
+    size_t overl_count;
+    std::string description;
+    std::string command_name;
+
+    virtual int process(const std::string argv[], int argc) { return -1; }
+};
+
+#endif
