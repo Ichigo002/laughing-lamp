b865e641fa6f47ee9f26ad88d9640bc21b446708
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2022-09-05 08:00:49 +0200

    add project

---
+++ laughing-lamp/src/TextureManager.h
@@ -0,0 +1,17 @@
+#include <SDL.h>
+#include <SDL_image.h>
+
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+class TextureManager
+{
+public:
+    static SDL_Texture* load(SDL_Renderer* renderer, const char* filename)
+    {
+        SDL_Surface* tmpSrf = IMG_Load(filename);
+        return SDL_CreateTextureFromSurface(renderer, tmpSrf);
+    }
+};
+
+#endif
---
+++ laughing-lamp/src/Vector2Int.h
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#ifndef VECTOR2INT_H
+#define VECTOR2INT_H
+
+class Vector2Int
+{
+public:
+    Vector2Int();
+    Vector2Int(int x, int y);
+
+    void set(int x, int y);
+
+    friend Vector2Int& operator+(Vector2Int& vec1, const Vector2Int& vec2);
+    friend Vector2Int& operator-(Vector2Int& vec1, const Vector2Int& vec2);
+    friend Vector2Int& operator*(Vector2Int& vec1, const Vector2Int& vec2);
+    friend Vector2Int& operator/(Vector2Int& vec1, const Vector2Int& vec2);
+    friend std::ostream& operator<<(std::ostream& stream, const Vector2Int& vec);
+
+    int x;
+    int y;
+};
+
+#endif
