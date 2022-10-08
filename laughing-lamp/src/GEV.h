60e7ec01caf002cc8696adbbea20de549b2007c3
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2022-10-08 21:30:09 +0200

    add definitions of vars

--- laughing-lamp/src/GEV.h
+++ laughing-lamp/src/GEV.h
@@ -1,13 +1,16 @@
 #ifndef GEV_H
 #define GEV_H
 
-//#include "utility/Vector2Int.h"
+#include "utility/Vector2Int.h"
 #include "SDL_rect.h"
 #include "SDL_render.h"
 
 /// What is __declspec(selectany)??
 /// This tells the linker to pick one definition for use by all external references and to discard the rest. [VS2019 Linker Tools Error LNK2005]
 #define DSPEC __declspec(selectany)
+#define vector Vector2Int
+#define rect SDL_Rect
+#define color SDL_Color
 
 using std::string;
 
@@ -18,11 +21,11 @@ namespace GEV
 {
 //--- Item's settings
 	// [ MORE SETTINGS => GUI/UIInventory.cpp ]
-	DSPEC SDL_Rect	item_size = { 0, 0, 32, 32 }; // Size of items
-	DSPEC int		item_dropped_rsc = 1.3; // Scale of dropped item
-	DSPEC int		item_pt_font_size = 13; // Size of small numbers of amount in one stack below items
-	DSPEC string    item_font_path = "assets/fonts/Gemunu/GemunuLibre-Bold.ttf"; // Path to font fot small nubmers below items
-	DSPEC SDL_Color item_font_color = { 255, 255, 255 }; // Font color of numbers below items
+	DSPEC rect	 item_size = { 0, 0, 32, 32 }; // Size of items
+	DSPEC int	 item_dropped_rsc = 1.3; // Scale of dropped item
+	DSPEC int	 item_pt_font_size = 13; // Size of small numbers of amount in one stack below items
+	DSPEC string item_font_path = "assets/fonts/Gemunu/GemunuLibre-Bold.ttf"; // Path to font fot small nubmers below items
+	DSPEC color  item_font_color = { 255, 255, 255 }; // Font color of numbers below items
 
 //--- User Interface
 	// UIInventory
